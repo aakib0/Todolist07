fa767bf9ae4c74a99c2656715e55b13a524e3163
aakib0 <javedaakib518@gmail.com>
2025-07-30 10:50:32 +0530

    place Clear All Tasks Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react'
+import React,{ useState, useEffect} from 'react'
 import AddTaskForm from "./components/AddTaskForm";
 import TaskList from "./components/TaskList"
 
@@ -20,13 +20,23 @@ const App = () => {
     );
     setTasks(updatedTasks);
   };
-
+ const  clearTasks = () => {
+  setTasks([]);
+ }
   return (
     <>
     <div className='min-h-screen bg-gray-700 flex flex-col items-center py-10 px-4'>
       <h1 className='text-4xl font-bold mb-6 text-blue-600'>My To-Do App</h1>
     <AddTaskForm onAdd={addTask}/>
+
     <TaskList tasks={tasks} onDelete={deleteTask} onEdit={editTask} />
+        {tasks.length > 0 && (
+      <button 
+      onClick={clearTasks}
+      className='mt-4 bg-amber-100 font-bold-400 rounded-2xl'>
+        Clear All Tasks
+      </button>
+    )}
     </div>
     </>
   )
